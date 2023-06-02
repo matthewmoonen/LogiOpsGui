bf962417a43e60aea5568db0317a9c7438e0cddc
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2023-06-02 17:41:06 +1000

    Two boxes created, which now print box + keypress to terminal

--- keypresses.py
+++ keypresses.py
@@ -1,18 +1,38 @@
 import tkinter as tk
 
+selected_box = None
+
 def on_keypress(event):
     if event.keysym == 'Escape':
         root.quit()
     else:
-        print("Key pressed:", event.keysym)
+        if selected_box is not None:
+            print("Box {} - Key pressed: {}".format(selected_box, event.keysym))
+
+def on_box1_click():
+    global selected_box
+    selected_box = 1
+    print("Box 1 selected")
+
+def on_box2_click():
+    global selected_box
+    selected_box = 2
+    print("Box 2 selected")
 
 def create_window():
+    global root
     root = tk.Tk()
     root.geometry("300x200")
-    root.title("Keypress Logger")
+    root.title("Click and Keypress Logger")
 
-    label = tk.Label(root, text="Press any key:")
+    label = tk.Label(root, text="Click a box and press a key:")
     label.pack(pady=10)
+
+    box1 = tk.Button(root, text="Box 1", command=on_box1_click)
+    box1.pack(pady=5)
+
+    box2 = tk.Button(root, text="Box 2", command=on_box2_click)
+    box2.pack(pady=5)
 
     root.bind("<Key>", on_keypress)
     root.mainloop()
