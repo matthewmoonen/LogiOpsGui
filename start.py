d657fd7d6281589e613737a3cb5247cd8ad0eb39
Matthew Moonen <matthewmoonen@gmail.com>
2023-07-20 15:27:37 +1000

    Create containers for each on the devices' buttons

--- start.py
+++ start.py
@@ -24,27 +24,99 @@ conn.commit()
 
 
 class LogitechDevice:
-    def __init__(self, name, min_dpi, max_dpi, default_dpi):
+    def __init__(self, name, min_dpi, max_dpi, default_dpi, buttons):
         self.name = name
         self.min_dpi = min_dpi
         self.max_dpi = max_dpi
         self.default_dpi = default_dpi
+        self.buttons = buttons
 
 # Creating instances of LogitechDevice for each device
 devices = [
-    LogitechDevice("MX Master 3", 200, 4000, 1000),
-    LogitechDevice("MX Master 3 for Mac", 200, 4000, 1000),
-    LogitechDevice("MX Master 2S", 200, 4000, 1000),
-    LogitechDevice("MX Master", 400, 1600, 1000),
-    LogitechDevice("MX Anywhere 2S", 200, 4000, 1000),
-    LogitechDevice("MX Anywhere 3", 200, 4000, 1000),
-    LogitechDevice("MX Vertical", 400, 4000, 1000),
-    LogitechDevice("MX Ergo", 512, 2048, 1000),
-    LogitechDevice("MX Ergo M575", 400, 2000, 1000),
-    LogitechDevice("M720 Triathlon", 200, 3200, 1000),
-    LogitechDevice("M590 Multi-Device Silent", 1000, 2000, 1000),
-    LogitechDevice("M500s Advanced Corded Mouse", 200, 4000, 1000),
+    LogitechDevice("MX Master 3", 200, 4000, 1000, ["0x0050", "0x0051", "0x0052", "0x0053", "0x0056", "0x00c3"]),
+    LogitechDevice("MX Master 3 for Mac", 200, 4000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Master 2S", 200, 4000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Master", 400, 1600, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Anywhere 2S", 200, 4000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Anywhere 3", 200, 4000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Vertical", 400, 4000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Ergo", 512, 2048, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("MX Ergo M575", 400, 2000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("M720 Triathlon", 200, 3200, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("M590 Multi-Device Silent", 1000, 2000, 1000, ["0x0050", "0x0051"]),
+    LogitechDevice("M500s Advanced Corded Mouse", 200, 4000, 1000, ["0x0050", "0x0051"]),
 ]
+
+
+class Control:
+    def __init__(self, control_id, function):
+        self.control_id = control_id
+        self.function = function
+
+table_data = [
+    Control('0x0050', 'Left Mouse Button'),
+    Control('0x0051', 'Right Mouse Button'),
+    Control('0x0052', 'Middle Mouse Button'),
+    Control('0x0053', 'Back Button'),
+    Control('0x0054', 'Back Button'),
+    Control('0x0056', 'Forward Button'),
+    Control('0x0057', 'Forward Button'),
+    Control('0x005b', 'Left Scroll'),
+    Control('0x005d', 'Right Scroll'),
+    Control('0x006e', 'Show Desktop'),
+    Control('0x006f', 'Lock Screen'),
+    Control('0x0090', 'Minimize'),
+    Control('0x0091', 'Maximize'),
+    Control('0x0095', 'Switch Screens'),
+    Control('0x00ba', 'Switch Apps'),
+    Control('0x00bb', 'Home'),
+    Control('0x00bc', 'Menu'),
+    Control('0x00bd', 'Back Button'),
+    Control('0x00be', 'Generic'),
+    Control('0x00bf', 'Print Screen'),
+    Control('0x00c0', 'Generic'),
+    Control('0x00c1', 'Generic'),
+    Control('0x00c2', 'Lock Screen'),
+    Control('0x00c3', 'Gesture Button'),
+    Control('0x00c4', 'Toggle SmartShift'),
+    Control('0x00c7', 'Decrease Brightness'),
+    Control('0x00c8', 'Increase Brightness'),
+    Control('0x00cc', 'Switch Apps'),
+    Control('0x00ce', 'Back Button'),
+    Control('0x00cf', 'Forward Button'),
+    Control('0x00d0', 'Switch Apps'),
+    Control('0x00d1', 'Generic'),
+    Control('0x00d2', 'Generic'),
+    Control('0x00d3', 'Generic'),
+    Control('0x00d4', 'Search'),
+    Control('0x00d5', 'Home'),
+    Control('0x00d6', 'Menu'),
+    Control('0x00d7', 'Switch Receivers'),
+    Control('0x00dd', 'Select Language'),
+    Control('0x00e0', 'Task View'),
+    Control('0x00e1', 'Action Center'),
+    Control('0x00e2', 'Decrease Backlight'),
+    Control('0x00e3', 'Increase Backlight'),
+    Control('0x00e4', 'Previous Track'),
+    Control('0x00e5', 'Play/Pause'),
+    Control('0x00e6', 'Next Track'),
+    Control('0x00e7', 'Mute'),
+    Control('0x00e8', 'Volume Down'),
+    Control('0x00e9', 'Volume Up'),
+    Control('0x00ea', 'App Menu'),
+    Control('0x00ed', 'Trackball Sensitivity?'),
+    Control('0x00ef', 'F key'),
+    Control('0x00f0', 'F key'),
+    Control('0x00f1', 'F key'),
+    Control('0x00f2', 'F key'),
+    Control('0x00f3', 'F key'),
+    Control('0x00f4', 'F key'),
+    Control('0x00f5', 'F key'),
+    Control('0x00f6', 'F key'),
+    Control('0x00fd', 'Mouse Sensitivity'),
+    Control('0x00fe', 'Home')
+]
+
 
 # Function to save user settings to the database
 def save_user_settings():
@@ -141,14 +213,14 @@ on_checkbox.grid(row=0, column=0, padx=5, pady=5)
 threshold_value = tk.StringVar(value="30")  # Default value for threshold
 threshold_label = ttk.Label(master=smartshift_frame, text="Threshold")
 threshold_label.grid(row=0, column=1, padx=5, pady=5)
-threshold_spinbox = ttk.Spinbox(master=smartshift_frame, from_=0, to=100, textvariable=threshold_value, validate="key")
+threshold_spinbox = ttk.Spinbox(master=smartshift_frame, from_=0, to=255, textvariable=threshold_value, validate="key")
 threshold_spinbox.grid(row=0, column=2, padx=5, pady=5)
 
 # Create torque input
 torque_value = tk.StringVar(value="50")  # Default value for torque
 torque_label = ttk.Label(master=smartshift_frame, text="Torque")
 torque_label.grid(row=1, column=1, padx=5, pady=5)
-torque_spinbox = ttk.Spinbox(master=smartshift_frame, from_=0, to=100, textvariable=torque_value, validate="key")
+torque_spinbox = ttk.Spinbox(master=smartshift_frame, from_=0, to=255, textvariable=torque_value, validate="key")
 torque_spinbox.grid(row=1, column=2, padx=5, pady=5)
 
 # Configure threshold and torque spinbox widgets
@@ -231,6 +303,86 @@ target_checkbox.grid(row=0, column=2, padx=5, pady=5)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create a dictionary to store the button containers
+button_containers = {}
+
+# Function to update the button containers based on the selected device
+def update_button_containers():
+    selected_device_now = selected_device.get()
+
+    # Remove existing button containers
+    for container in button_containers.values():
+        container.destroy()
+
+    button_containers.clear()
+
+    # Create button containers for the selected device
+    for device in devices:
+        if device.name == selected_device_now:
+            for button in device.buttons:
+                container = ttk.LabelFrame(master=window, text=button)
+                container.pack(pady=5)
+                button_containers[button] = container
+
+                button1 = ttk.Button(master=container, text="Button 1")
+                button1.pack(padx=5, pady=5)
+
+                button2 = ttk.Button(master=container, text="Button 2")
+                button2.pack(padx=5, pady=5)
+
+
+# Update the button containers initially
+update_button_containers()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Create dpi input
 dpi_value = tk.StringVar(value="1000")  # Default value for dpi
 dpi_label = ttk.Label(master=window, text="DPI")
@@ -271,10 +423,51 @@ def update_dpi_on_selection(event):
     selected_device_index = dropdown.current()
     selected_device = devices[selected_device_index]
     dpi_value.set(str(selected_device.default_dpi))
+    update_button_containers()
 
 
 # Bind the function to the <<ComboboxSelected>> event
 dropdown.bind("<<ComboboxSelected>>", update_dpi_on_selection)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Load the user settings from the database
