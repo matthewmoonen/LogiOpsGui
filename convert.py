60aaa0e2f8bb79debd897692444636c036d71f6b
Matthew Moonen <matthewmoonen@gmail.com>
2023-07-20 17:46:49 +1000

    Created convert function to match upper/lowercase keypress

--- convert.py
+++ convert.py
@@ -1,24 +1,34 @@
-# grave = asciitilde
-# 1 = exclam
-# 2 = at
-# 3 = numbersign
-# 4 = dollar
-# 5 = percent
-# 6 = asciicircum
-# 7 = ampersand
-# 8 = asterisk
-# 9 = parenleft
-# 0 = parenright
-# minus = underscore
-# equal = plus
+def main():
+    print(get_keymate('at', 'en_US'))
 
-# bracketleft = braceleft
-# bracketright = braceright
-# backslash = bar
-# semicolon = colon
-# apostrophe = quotedbl
-# comma = less
-# period = greater
-# slash = question
+def get_keymate(key, layout):
 
+    keymates_en_us = {
+        'asciitilde': 'grave',
+        'exclam': '1',
+        'at': '2',
+        'numbersign': '3',
+        'dollar': '4',
+        'percent': '5',
+        'asciicircum': '6',
+        'ampersand': '7',
+        'asterisk': '8',
+        'parenleft': '9',
+        'parenright': '0',
+        'underscore': 'minus',
+        'plus': 'equal',
+        'braceleft': 'bracketleft',
+        'braceright': 'bracketright',
+        'bar': 'backslash',
+        'colon': 'semicolon',
+        'quotedbl': 'apostrophe',
+        'less': 'comma',
+        'greater': 'period',
+        'question': 'slash'
+    }
 
+    if layout == 'en_US':
+        return keymates_en_us.get(key, key)
+
+if __name__ == "__main__":
+    main()
