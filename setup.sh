62e370758aa647b6ad3503acf9d7e9f5845133cf
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2024-07-06 17:33:20 +1200

    Add setup, requirements files

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the application
+python logui.py
