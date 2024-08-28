24ef29d9aa139918eb391d0d5a769310318788a9
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2024-08-29 02:37:34 +1200

    delete unnecessary files

--- needs_super.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def is_root():
-    return os.geteuid() == 0
-def check_root():
-    if is_root():
-        print("The script is running as root.")
-    else:
-        print("The script is not running as root.")
